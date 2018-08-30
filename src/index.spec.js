7ba22a51821ab9c37a2277b5f112349ed22db47e
Louis Cospain <elcoosp@gmail.com>
2018-08-30 02:23:32 +0200

    test: 💍 Correct test sentences

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,18 +5,18 @@ const matrix = [[0, 1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 const T = Traverser()
 
-test("should traverse starting fromTopLeft", () => {
+test("Should traverse starting from top left", () => {
 	expect([...T.fromTopLeft(matrix)]).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 })
 
-test("should traverse starting fromTopRight", () => {
+test("Should traverse starting from top right", () => {
 	expect([...T.fromTopRight(matrix)]).toEqual([3, 2, 1, 0, 6, 5, 4, 9, 8, 7])
 })
 
-test("should traverse starting fromBottomLeft", () => {
+test("Should traverse starting from bottom left", () => {
 	expect([...T.fromBottomLeft(matrix)]).toEqual([7, 8, 9, 4, 5, 6, 0, 1, 2, 3])
 })
 
-test("should traverse starting fromBottomRight", () => {
+test("Should traverse starting from bottom right", () => {
 	expect([...T.fromBottomRight(matrix)]).toEqual([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
 })
